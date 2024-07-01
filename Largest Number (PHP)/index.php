0219f5b4fe7361567e8825f075f8649c943832c5
Asraflab <asraflab@gmail.com>
2024-07-02 00:41:37 +0600

    Assignment Module 5

---
+++ Largest Number (PHP)/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Profile Card</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div>
+        <h1>Find the largest number using PHP </h1>
+        <?php
+        $num1 = 4;
+        $num2 = 5;
+        $num3 = 6; ?>
+
+    <p><?php echo "First number is : " .$num1 ?> </p>
+    <p> <?php echo "First number is : " .$num2 ?>  </p>
+    <p> <?php echo "First number is : " .$num3 ?> </p>
+    <?php
+        if ($num1 >= $num2 && $num1 >= $num3) {
+            echo "<p>Largest Number is: " . $num1 . "</p>";
+        } elseif ($num2 >= $num1 && $num2 >= $num3) {
+            echo "<p>Largest Number is: " . $num2 . "</p>";
+        } else {
+            echo "<h2>Largest Number is: " . $num3 . "</h2>";
+        }
+        ?>
+
+
+
+    </div>
+</body>
+
+</html>
